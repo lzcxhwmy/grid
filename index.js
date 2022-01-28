13a8b5f4bf744a6f168eb0cd3284a2ad1adc7357
lizhicheng <lizhicheng@wudi.xyz>
2022-01-28 21:24:48 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+// 导航实例
+const headerEl = document.querySelector("header");
+// 返回顶部实例
+const scrollToTop = document.querySelector(".scrollToTop");
+
+// 窗口滚动处理
+window.addEventListener("scroll", () => {
+  // 固定导航
+  let height = headerEl.getBoundingClientRect().height;
+
+  if (window.pageYOffset - height > 800) {
+    if (!headerEl.classList.contains("sticky")) {
+      headerEl.classList.add("sticky");
+    }
+  } else {
+    headerEl.classList.remove("sticky");
+  }
+
+  // 显示返回顶部
+  if (window.pageYOffset > 2000) {
+    scrollToTop.style.display = "block";
+  } else {
+    scrollToTop.style.display = "none";
+  }
+});
+
+// 初始轮播
+new Glide(".glide").mount();
+
+
+
+// 滚动展示插件
+// 通用动画配置，从底部50象素滑出来
+const staggeringOption = {
+  delay: 300,
+  distance: "50px",
+  duration: 500,
+  easing: "ease-in-out",
+  origin: "bottom"
+};
+// 滚动到业务流程时的展示动画，interval需要单独设置，每个feature元素相继350毫秒，下同
+ScrollReveal().reveal(".feature", { ...staggeringOption, interval: 350 });
+ScrollReveal().reveal(".service-item", { ...staggeringOption, interval: 350 });
+
+
+/* ***** 响应式**** */
+
+// 折叠按钮
+const burgerEl = document.querySelector(".burger");
+const nav = document.querySelector("header nav");
+burgerEl.addEventListener("click", () => {
+  headerEl.classList.toggle("open");
+});
+
+  
